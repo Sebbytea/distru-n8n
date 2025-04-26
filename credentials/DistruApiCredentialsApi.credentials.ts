0bb9b3dbb590c093c16ab7728847230e3e438b7f
Sebbytea <Sebastian.tidwell@gmail.com>
2025-04-26 15:32:36 -0500

    chore: bump version to 0.1.17 for build fixes

--- credentials/DistruApiCredentialsApi.credentials.ts
+++ credentials/DistruApiCredentialsApi.credentials.ts
@@ -1,24 +1,55 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
+import {
+	ICredentialType,
+	INodeProperties,
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+} from 'n8n-workflow';
 
-export class DistruApiCredentialsApi implements ICredentialType {
-  name = 'distruApi';
-  displayName = 'Distru API';
-  documentationUrl = 'https://apidocs.distru.dev';
-  properties: INodeProperties[] = [
-    {
-      displayName: 'API Token',
-      name: 'apiToken',
-      type: 'string',
-						typeOptions: { password: true },
-      default: '',
+export class DistruApiCredentials implements ICredentialType {
+	name = 'distruApi';
+	displayName = 'Distru API';
+	documentationUrl = 'https://apidocs.distru.dev';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Token',
+			name: 'apiToken',
+			type: 'string',
+			default: '',
       description: 'Your Distru API Bearer token',
-    },
-    {
-      displayName: 'Use Staging Environment',
-      name: 'useStaging',
-      type: 'boolean',
-      default: false,
+			required: true,
+		},
+		{
+			displayName: 'Use Staging Environment',
+			name: 'useStaging',
+			type: 'boolean',
       description: 'Toggle to use Distru staging environment instead of production',
-    },
-  ];
+			default: false,
+		},
+	];
+
+	/**
+	 * Declare how to add auth to requests.
+	 * Here, bearer token in Authorization header.
+	 */
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: 'Bearer {{$credentials.apiToken}}',
+			},
+		},
+	};
+
+	/**
+	 * The test object describes how n8n should verify this credential.
+	 * It leverages credential data via interpolation syntax.
+	 */
+	test: ICredentialTestRequest = {
+		request: {
+			// baseURL is conditional on staging toggle
+			baseURL:
+				'={{$credentials.useStaging ? "https://staging.distru.com/public/v1" : "https://app.distru.com/public/v1"}}',
+			url: '/products?page[number]=1&page[size]=1',
+		},
+	};
 }
