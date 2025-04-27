7f8c09ade0ffd961b81ef612c6ac17d37714eb8e
Sebbytea <Sebastian.tidwell@gmail.com>
2025-04-27 17:29:01 -0500

    chore: successful prepublish, ready to publish

--- nodes-base/nodes/ReadPdf/test/ReadPDF.test.ts
+++
@@ -1,7 +0,0 @@
-import { getWorkflowFilenames, testWorkflows } from '@test/nodes/Helpers';
-
-describe('Test Read PDF Node', () => {
-	const workflows = getWorkflowFilenames(__dirname);
-
-	testWorkflows(workflows);
-});
